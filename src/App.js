697a184172636e47cd3c5e2f8cf5196bbaf23e2a
tuantranquang20 <57103617+tuantranquang20@users.noreply.github.com>
2022-09-07 11:19:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ansible auto setup server with nginx.
+          Jenkins build
         </p>
       </header>
     </div>
